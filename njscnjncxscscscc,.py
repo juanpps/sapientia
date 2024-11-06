ef8e8c747921a41e820670960860d8906531320f
juanpps <migueljuanguerrerot@arquidiocesanos.edu.co>
2024-11-06 14:50:06 -0500

    Add files via upload

---
+++ njscnjncxscscscc,.py
@@ -0,0 +1,6 @@
+nombre= input("introdusca su nombre\n")
+
+apellido= input("introdusca su apellido\n")
+
+print("su nombre es " + nombre + " y su  apellido es " + apellido + " y usted es oficialmente el mas gay del colegio")
+
